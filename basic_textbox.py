ff4aa301e0eb243e44090fbd5ea8b5e38159940f
FatsoBoo <53587521+FatsoBoo@users.noreply.github.com>
2020-06-06 23:34:24 -0400

    Add files via upload

--- basic_textbox.py
+++ basic_textbox.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, flash, request, url_for, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'verysecretkey'
 
-@app.route('/')
-def hello_world():
-    return "Hello OM!"
+class QNABox(FlaskForm):
+    question = StringField('What is your question?', validators=[DataRequired()])
+    submit = SubmitField('Ask')
+
+@app.route('/qa')
+def ask():
+    form = QNABox()
+    return render_template('textbox.html', title='LALLALA', form=form)
+
+
+
